df85bab87d0a18dcf361fa9b5d565ede2d2b223f
skylar430 <edson.gmax.ahimbisibwe@gmail.com>
2023-12-15 02:38:27 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts formatie
- *
- * Return: 0
+ * main - this is the main function
+ * Return: always 0
  */
-int main(void)
+int mani(void)
 {
-puts("\"Programming is like building a multilingual puzzle \n");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
